26f2a47a529b583af04654f48edd9511aec727b2
zhangruichang <zhangruichang112@gmail.com>
2015-01-09 02:21:33 +0800

    Search In Rotated Array Concise Code

---
+++ SearchInRotatedArray.cpp
@@ -0,0 +1,53 @@
+https://oj.leetcode.com/problems/search-in-rotated-sorted-array-i/
+class Solution {
+public:
+    int search(int A[], int n, int x) {
+        int low=0, high=n-1;
+        while(low<=high)
+        {
+            if(low+1==high) return A[low] == x ? low : (A[high]==x ? high : -1);//两个元素单独判,一个元素后面可以处理
+            int mid=(low+high)/2;
+            if(A[mid]==x) return mid;
+            if(A[mid] > A[low])
+            {
+                if(A[low]<=x&&x<A[mid]) high=mid-1;//进行优化，只有x在左边，才搜左边，注意等号
+                else low=mid+1;//否则都右边
+            }
+            else
+            {
+                if(A[mid] < x && x<= A[high]) low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+https://oj.leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Solution {
+public:
+    bool search(int A[], int n, int x) {
+        int low=0, high=n-1;
+        while(low<=high)
+        {
+            if(low+1==high) return A[low] == x ? 1 : (A[high]==x ? 1 : 0);//和上面类似思想
+            int mid=(low+high)/2;
+            if(x==A[mid]) return 1;
+            if(A[mid] > A[low])
+            {
+                if(A[low] <=x && x<A[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else if(A[mid] < A[low])
+            {
+                if(A[mid] < x && x<=A[high]) low=mid+1;
+                else high=mid-1;
+            }
+            else
+                low++;//不能logn的原因
+        }
+        return 0;
+    }
+};
