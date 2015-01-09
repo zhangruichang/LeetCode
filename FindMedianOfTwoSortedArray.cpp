e06d4241cc0ed7e8524de1a6a8d8c6449b4ceb43
zhangruichang <zhangruichang112@gmail.com>
2015-01-09 19:46:17 +0800

    Find Median usig Find Kth ele

--- FindMedianOfTwoSortedArray.cpp
+++ FindMedianOfTwoSortedArray.cpp
@@ -1,62 +1,24 @@
-#include<stdio.h>
-
-int getMedianRec(int ar1[], int ar2[], int left, int right, int n);
-
-/* This function returns median of ar1[] and ar2[].
-   Assumptions in this function:
-   Both ar1[] and ar2[] are sorted arrays
-   Both have n elements */
-int getMedian(int ar1[], int ar2[], int n)
-{
-    return getMedianRec(ar1, ar2, 0, n-1, n);
-}
-
-/* A recursive function to get the median of ar1[] and ar2[]
-   using binary search */
-int getMedianRec(int ar1[], int ar2[], int left, int right, int n)
-{
-    int i, j;
-
-    /* We have reached at the end (left or right) of ar1[] */
-    if (left > right)
-        return getMedianRec(ar2, ar1, 0, n-1, n);
-
-    i = (left + right)/2;
-    j = n - i - 1;  /* Index of ar2[] */
-
-    /* Recursion terminates here.*/
-    if (ar1[i] > ar2[j] && (j == n-1 || ar1[i] <= ar2[j+1]))
-    {
-        /* ar1[i] is decided as median 2, now select the median 1
-           (element just before ar1[i] in merged array) to get the
-           average of both*/
-        if (i == 0 || ar2[j] > ar1[i-1])
-            return (ar1[i] + ar2[j])/2;
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {//m>0 || n>0
+        int tt=m+n;
+        if(tt & 1)
+            return findKth(A, m, B, n, tt/2+1);//odd, one ele
         else
-            return (ar1[i] + ar1[i-1])/2;
+            return double(findKth(A,m,B,n,tt/2)+findKth(A,m,B,n,tt/2+1))/2;//even, average of two elems
     }
-
-    /*Search in left half of ar1[]*/
-    else if (ar1[i] > ar2[j] && j != n-1 && ar1[i] > ar2[j+1])
-        return getMedianRec(ar1, ar2, left, i-1, n);
-
-    /*Search in right half of ar1[]*/
-    else /* ar1[i] is smaller than both ar2[j] and ar2[j+1]*/
-        return getMedianRec(ar1, ar2, i+1, right, n);
-}
-
-/* Driver program to test above function */
-int main()
-{
-    int ar1[] = {1, 12, 15, 26, 38};
-    int ar2[] = {2, 13, 17, 30, 45};
-    int n1 = sizeof(ar1)/sizeof(ar1[0]);
-    int n2 = sizeof(ar2)/sizeof(ar2[0]);
-    if (n1 == n2)
-        printf("Median is %d", getMedian(ar1, ar2, n1));
-    else
-        printf("Doesn't work for arrays of unequal size");
-
-    getchar();
-    return 0;
-}
+    int findKth(int A[], int m, int B[], int n, int k)
+    {
+        if(m>n) return findKth(B,n, A, m, k);//ensure m<=n
+        if(!m && n) return B[k-1];//one empty, directly return
+        else if(m && !n) return A[k-1];
+        if(k==1) return min(A[0], B[0]);//k=1, put behind, ensure two non-empty array
+        int pa=min(k/2, m), pb=k-pa;//ensure pa in range, and n>mm thus pa<=m, pb>=k/2
+        if(A[pa-1] < B[pb-1])
+            return findKth(A+pa, m-pa, B, n, k-pa);//delete A firt part
+        else if(A[pa-1] > B[pb-1])
+            return findKth(A, m, B+pb, n-pb, k-pb);//delete B first part, as B first part do not have kth eles
+        else
+            return A[pa-1];//directly return
+    }
+};
