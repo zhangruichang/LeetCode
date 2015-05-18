a8c666a194ed8a9b6d4d363981a3ec7e169633c8
zhangruichang <zhangruichang112@gmail.com>
2015-05-18 22:58:06 +0800

    Course Schedule Toposort

---
+++ CourseSchedule/main.cpp
@@ -0,0 +1,108 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int a[maxn], n, t, m;
+
+class Solution {
+public:
+    bool canFinish(int n, vector<pair<int, int>>& prerequisites) {
+        bool A[n][n];
+        int indegree[n];
+        memset(A, 0, sizeof A);
+        for(auto e: prerequisites) A[e.second][e.first]=1;
+        for(int j=0;j<n;j++)
+        {
+            int cnt=0;
+            for(int i=0;i<n;i++)
+                cnt+=A[i][j];
+            indegree[j]=cnt;
+            //cout<<cnt<<endl;
+        }
+        for(int i=0;i<n;i++)
+        {
+            int j;
+            for(j=0;j<n;j++)
+            {
+                if(!indegree[j]) break;
+            }
+            if(j>=n) return 0;
+            indegree[j]=-1;//mark as delete from graph;
+            for(int k=0;k<n;k++) if(A[j][k]) indegree[k]--;
+        }
+        return 1;
+    }
+} S;
+
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    vector<pair<int, int>> pre={};
+    int n=1;
+    cout<<S.canFinish(n, pre)<<endl;
+	return 0;
+}
