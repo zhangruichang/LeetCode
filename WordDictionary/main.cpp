8284967acc85986f62d995bca67c209929401e3b
zhangruichang <zhangruichang112@gmail.com>
2015-05-22 22:31:15 +0800

    KMP Next array, reverse string append

---
+++ WordDictionary/main.cpp
@@ -0,0 +1,136 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int a[maxn], n, t, m;
+
+class WordDictionary {
+
+public:
+    struct Node
+    {
+        bool isWord;
+        Node* Child[26];
+        Node()
+        {
+            isWord=0;
+            for(int i=0;i<26;i++) Child[i]=NULL;
+        }
+    };
+    WordDictionary()
+    {
+        root=new Node();
+    }
+    // Adds a word into the data structure.
+    void addWord(string word) {
+        Node* p=root;
+        for(auto e : word)
+        {
+            int index=e-'a';
+            if(!p->Child[index]) p->Child[index]=new Node();
+            p=p->Child[index];
+        }
+        p->isWord=1;
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    bool search(string word) {
+        return dfs(0, root, word);
+    }
+    bool dfs(int i, Node* p, const string& word)
+    {
+        //Node* p=root;
+        if(i==word.size() && p->isWord) return 1;
+        char e=word[i];
+        if(e=='.')
+        {
+            for(int j=0;j<26;j++)
+            {
+                if(p->Child[j] && dfs(i+1, p->Child[j], word)) return 1;
+            }
+            return 0;
+        }
+        else return p->Child[e-'a'] && dfs(i+1, p->Child[e-'a'], word);
+    }
+private:
+    Node* root;
+};
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary;
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    WordDictionary wordDictionary;
+    wordDictionary.addWord("word");
+    //cout<<"zhang";
+    cout<<wordDictionary.search("word");
+	return 0;
+}
