4c8751d763a81fb14027f26515ae59942d44904a
zhangruichang <zhangruichang112@gmail.com>
2015-06-01 20:08:57 +0800

    Find Largest Ele

---
+++ KthLargest/main.cpp
@@ -0,0 +1,106 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int n, t, m;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        return QS(nums, 0, n-1, n-k+1);
+    }
+    int Partition(vector<int>& a, int low, int high)
+    {
+        int i = low, j = high, pivot=a[i];
+        while( i < j)
+        {
+            while( i < j && a[j] >= pivot) j--;
+            while( i < j && a[i] <= pivot) i++;
+            if(i < j) swap(a[i], a[j]);
+        }
+        swap(a[low], a[i]);
+        return i;
+    }
+    int QS(vector<int>& num, int low, int high, int k)//1<=k<=high-low+1
+    {
+        if(low>high || k>high-low+1) return -1;
+        int mid=Partition(num, low, high);
+        int order=mid-low+1;
+        if(k==order) return num[mid];
+        else if(order>k) return QS(num, low, mid-1, k);
+        else return QS(num, mid+1, high, k-order);
+    }
+} S;
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    //Solution S;
+    vector<int> v={2, 1};
+    cout<<S.findKthLargest(v,1)<<endl;
+	return 0;
+}
