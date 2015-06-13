7a0405c2ec961ffeb23c30a5e16f208263cd9a89
zhangruichang <zhangruichang112@gmail.com>
2015-06-14 01:41:11 +0800

    InvertBinaryTree

---
+++ MaxSubArraySum/main.cpp
@@ -0,0 +1,115 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int a[maxn], n, t, m;
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int> &a)
+    {
+        int n=a.size();
+        if(!n) return 0;
+        int sum=0;
+        int minpresum=sum, maxsubsum=a[0];
+        for(int i=1;i<=n;i++)
+        {
+            sum+=a[i-1];
+            maxsubsum=max(maxsubsum, sum-minpresum);
+            minpresum=min(minpresum, sum);
+        }
+        return maxsubsum;
+    }
+};
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
+        int dp, ans;
+        ans=dp=a[0];
+        for(int i=1;i<n;i++)
+        {
+            dp=max(dp+a[i], a[i]);
+            ans=max(ans, dp);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    cin>>t;
+    for(int ti=1;ti<=t;ti++)
+    {
+        cin
+        printf("Case #%d:\n", ti);
+        cout
+    }
+	return 0;
+}
